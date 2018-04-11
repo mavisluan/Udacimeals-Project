763c6a8e2c01039e8653d37ceb6968554b18799a
Yi Angelov <mavisluan83@gmail.com>
2018-04-10 21:52:11 -0700

    use mapStateToProps to pass the state to App as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+//import connect to pass the state and (dispatch)actions to React Components as props
+import { connect } from 'react-redux';
 class App extends Component {
-
   render() {
+    console.log('Props', this.props)
     return (
       <div>
         Hello World!
@@ -11,4 +12,17 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  return {
+     calendar: Object.keys(calendar)
+      .map((day) => ({
+        day,
+        meals: {...calendar[day]}
+      }))
+  }
+}
+
+
+//connect()() curried function
+//Pass mapStateToProps to call the first function and pass the state to App as props
+export default connect(mapStateToProps)(App);
