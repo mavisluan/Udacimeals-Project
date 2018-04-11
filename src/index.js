9337909eeff3826e4feaab2fc78267bc5dd1388b
Yi Angelov <mavisluan83@gmail.com>
2018-04-10 17:28:41 -0700

    pass a store prop to the App component

--- src/index.js
+++ src/index.js
@@ -12,5 +12,9 @@ const store = createStore(
   reducer,
    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  //pass a store prop to the App component that React renders
+  <App store = {store}/>,
+  document.getElementById('root')
+);
 registerServiceWorker();
