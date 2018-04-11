a3f70f323c0581fccaf1c5ebb815c50f79151b7a
Yi Angelov <mavisluan83@gmail.com>
2018-04-10 17:03:35 -0700

    create a redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//change the import directory because App was moved to components folder
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
+//import createStore and reducer
+import { createStore } from 'redux';
+import reducer from './reducers'
+//create a redux store
+const store = createStore(
+  reducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
