83fd449f6b254729dbedc506d7801eeb99335e10
Yi Angelov <mavisluan83@gmail.com>
2018-04-10 21:19:21 -0700

    Import {Provider}

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 //import createStore and reducer
 import { createStore } from 'redux';
+//import Provider component to pass data from store to any React components
+import { Provider } from 'react-redux';
 import reducer from './reducers'
 //create a redux store
 const store = createStore(
@@ -13,8 +15,11 @@ const store = createStore(
    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 ReactDOM.render(
-  //pass a store prop to the App component that React renders
-  <App store = {store}/>,
+  //Wrap the main react component (App) into Provider and then pass the store to Provider
+  //so that App component and all the components that App renders will get the data from the store.
+  <Provider store = {store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
