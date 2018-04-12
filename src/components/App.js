0fc6ec67ed26e769aa0a010d2e69d59e4bb3de18
Yi Angelov <mavisluan83@gmail.com>
2018-04-12 13:58:10 -0700

    update App mapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 }
 
-function mapStateToProps(calendar) {
+/*function mapStateToProps(calendar) {
   return {
      calendar: Object.keys(calendar)
       .map((day) => ({
@@ -22,7 +22,25 @@ function mapStateToProps(calendar) {
         meals: {...calendar[day]}
       }))
   }
+} */
+
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
 }
+
 
 function mapDispatchToProps(dispatch) {
   return {
